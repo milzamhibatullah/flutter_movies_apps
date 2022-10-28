9a5fe7582ed637286d8989de9cd6cbda3308c3b9
Milzam Hibatullah <hmilzam@gmail.com>
2022-10-28 23:44:01 +0700

    create separated view for all category movies

--- lib/presentation/home/home.screen.dart
+++ lib/presentation/home/home.screen.dart
@@ -2,7 +2,8 @@ import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:movies_apps/presentation/home/views/movieview_view.dart';
+import 'package:movies_apps/config.dart';
+import 'package:movies_apps/presentation/home/movie/views/movie_view.dart';
 
 import 'controllers/home.controller.dart';
 
@@ -11,12 +12,17 @@ class HomeScreen extends GetView<HomeController> {
   static final iconList = <IconData>[
     Icons.dashboard,
     Icons.explore,
-    Icons.person
+    Icons.watch_later_rounded
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.black,
+        title: Image.asset(AssetsEnv.APPSLOGO,height: Get.height/8,),
+      ),
       backgroundColor: Colors.black,
       bottomNavigationBar: ConvexAppBar(
         style: TabStyle.react,
@@ -24,13 +30,13 @@ class HomeScreen extends GetView<HomeController> {
         activeColor: Colors.white,
         backgroundColor: Colors.black,
         items: const [
-          TabItem(icon: Icons.home, title: 'Home',),
-          TabItem(icon: Icons.explore, title: 'Explore'),
-          TabItem(icon: Icons.person, title: 'Account')
+          TabItem(icon: Icons.movie, title: 'Movies',),
+          TabItem(icon: Icons.tv_rounded, title: 'TV show'),
+          TabItem(icon: Icons.watch_later_outlined, title: 'Watch list')
         ],
 
       ),
-      body: const MovieviewView(),
+      body: const MovieView(),
     );
   }
 }
--- lib/presentation/home/movie/views/movie_view.dart
+++ lib/presentation/home/movie/views/movie_view.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:movies_apps/presentation/component/text.component.dart';
+import 'package:movies_apps/presentation/home/movie/views/coming_soon_view.dart';
+import 'package:movies_apps/presentation/home/movie/views/now_playing_view.dart';
+import 'package:movies_apps/presentation/home/movie/views/popular_movie_view.dart';
+import 'package:movies_apps/presentation/home/movie/views/top_rated_view.dart';
 
 import '../controllers/movie_controller.dart';
 
@@ -10,14 +15,93 @@ class MovieView extends GetView<MovieController> {
   Widget build(BuildContext context) {
     controller.onInit();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MovieView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'MovieView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Color(0xFF212121),
+      body: SafeArea(
+        child: SizedBox(
+          width: Get.width,
+          height: Get.height,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Obx(()=>controller.isTopRatedLoading.value==true?const CircularProgressIndicator():Center(
+                //   child: Text(
+                //     '${controller.topRatedMovies.value.results!.length}',
+                //     style: const TextStyle(fontSize: 20),
+                //   ),
+                // )),
+                // const SizedBox(height: 20.0,),
+                // Obx(()=>controller.isNowPlayLoading.value==true?const CircularProgressIndicator():Center(
+                //   child: Text(
+                //     '${controller.nowPlayingMovies.value.results!.length}',
+                //     style: const TextStyle(fontSize: 20),
+                //   ),
+                // )),
+                // const SizedBox(height: 20.0,),
+                // Obx(()=>controller.isUpcomingLoading.value==true?const CircularProgressIndicator():Center(
+                //   child: Text(
+                //     '${controller.upcomingMovies.value.results!.length}',
+                //     style: const TextStyle(fontSize: 20),
+                //   ),
+                // )),
+                // const SizedBox(height: 20.0,),
+                // Obx(()=>controller.isPopularLoading.value==true?const CircularProgressIndicator():Center(
+                //   child: Text(
+                //     '${controller.popularMovies.value.results!.length}',
+                //     style: const TextStyle(fontSize: 20),
+                //   ),
+                // )),
+                const SizedBox(
+                  height: 20.0,
+                ),
+
+                ///coming soon section
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: comtext.title(
+                      text: 'Coming Soon', size: 18.0, color: Colors.white),
+                ),
+                const ComingSoonView(),
+                const SizedBox(
+                  height: 10.0,
+                ),
+
+                ///trending
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0),
+                  child: comtext.title(
+                      text: 'Trending', size: 18.0, color: Colors.white),
+                ),
+                const PopularMovieView(),
+
+                ///now playing
+                const SizedBox(
+                  height: 20.0,
+                ),
+
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: comtext.title(
+                        text: 'Now Playing', size: 18.0, color: Colors.white)),
+                const NowPlayingView(),
+
+                ///toprated
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: comtext.title(
+                        text: 'Top Rated', size: 18.0, color: Colors.white)),
+               const TopRatedView(),
+
+                const SizedBox(
+                  height: 40.0,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
